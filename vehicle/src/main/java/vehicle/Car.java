ddfaceafe381296aba15460ee4f9a7c2148d64f0
J-Tell <fairyq007@hotmail.com>
2023-03-10 10:38:02 -0500

    car is going

--- vehicle/src/main/java/vehicle/Car.java
+++ vehicle/src/main/java/vehicle/Car.java
@@ -14,15 +14,20 @@ abstract class Car {
         startingMileage = sm;
     }
 /** Starting mileage is 0. */
-    public Car(String make, String model)
+    public Car(String m, String mo)
     {
-        //
+        make = m;
+        model = mo;
+        startingMileage = 0;
     }
 /** If able to drive the full given number of miles, returns true. If
 not, returns false.
 @throws IllegalArgumentException if miles is negative.*/
     public boolean canDrive(double miles) {
-        //
+        if (miles <= startingMileage) {
+            return true;
+        }
+        return false;
     }
 /** Drives the full given number of miles.
 @throws IllegalArgumentException if miles is negative or if miles is
@@ -36,12 +41,12 @@ Mileage should be rounded to 1 decimal place. If mileage is a whole
 number, ".0" should still display.
 */
     public String toString() {
-        //
+        return "<" + make + " and " + model + "> (" + startingMileage + "> )";
     }
 /** Returns how many miles have been driven so far (odometer). */
     public double getMileage()
     {
-        //
+        return startingMileage;
     }
 /** Returns the make of the car. */
     public String getMake() {
@@ -53,10 +58,14 @@ number, ".0" should still display.
     }
 /** Returns how many more miles the car can currently go given the
 remaining fuel/energy reserves. */
-    public abstract double getRemainingRange()
+    public abstract double getRemainingRange() {
+        //
+    }
 /** Adds mileage to the odometer.
 @throws IllegalArgumentException if miles is negative. */
-    protected void addMileage(double miles);
+    protected void addMileage(double miles) {
+        startingMileage += miles;
+    }
 /** The car attempts to drive, in order, each of the daily number of
 miles in the list milesEachDay. Once the car cannot drive one of the
 day’s distance, no more days are attempted. Returns the number of
