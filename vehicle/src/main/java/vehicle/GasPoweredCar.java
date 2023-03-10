7459ba5081d2d6ea0df291da53b1ee2848da0c82
Faizan <baa98989@gmail.com>
2023-03-10 10:40:08 -0500

    added functions

---
+++ vehicle/src/main/java/vehicle/GasPoweredCar.java
@@ -0,0 +1,41 @@
+package vehicle;
+
+public abstract class GasPoweredCar extends Car{
+
+    private double fuelCapacityGallons;
+    private String make;
+    private double startingMileage;
+    private String model;
+
+    public GasPoweredCar(String make, String model, double startingMileage, double fuelCapacityGallons) {
+        
+        super(make, model, startingMileage);
+        make = this.make;
+        model = this.model;
+        startingMileage = this.startingMileage;
+        fuelCapacityGallons = this.fuelCapacityGallons;
+    }
+
+    public void drive(double miles) {
+        
+    }
+
+    public double getMPG() {
+        return this.startingMileage;
+    }
+
+    public double getFuelLevel() {
+        return 0.0;
+    }
+
+    public double getFuelCapacity() {
+        return this.fuelCapacityGallons;
+    }
+
+    public void refillTank(double gallons) {
+        if(gallons < 0 || gallons > this.fuelCapacityGallons)
+            throw new IllegalArgumentException();
+        //this.getFuelLevel() += gallons;
+    }
+    
+}
