bf9ae6ae2e1b6fa6653dc0160d41a561295e61c5
aBacIdEe <maxgong2007@gmail.com>
2023-03-10 10:47:14 -0500

    gaspoweredcar

--- vehicle/src/main/java/vehicle/GasPoweredCar.java
+++ vehicle/src/main/java/vehicle/GasPoweredCar.java
@@ -1,13 +1,23 @@
 package vehicle;
 
 abstract class GasPoweredCar extends Car {
+    double mpg;
+    double fuelCapacityGallons;
     
-    public GasPoweredCar(String make, String model, double startingMileage, double mpg, double fuelCapacityGallons);
+    public GasPoweredCar(String make, String model, double startingMileage, double m, double cap) {
+        super(make, model, startingMileage);
+        mpg = m;
+        fuelCapacityGallons = cap;
+    }
 
-    public GasPoweredCar(String make, String model, double mpg, double fuelCapacityGallons);
+    public GasPoweredCar(String make, String model, double m, double cap) {
+        super(make, model);
+        mpg = m;
+        fuelCapacityGallons = cap;
+    }
 
     public void drive(double miles) {
-        return
+        
     }
     
     public double getMPG() {
