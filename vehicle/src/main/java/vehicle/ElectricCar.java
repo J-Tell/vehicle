0568e90433460f1b3fc7f81632e0e6df79747475
J-Tell <fairyq007@hotmail.com>
2023-03-13 10:21:58 -0400

    elcetric car thingz?

--- vehicle/src/main/java/vehicle/ElectricCar.java
+++ vehicle/src/main/java/vehicle/ElectricCar.java
@@ -3,12 +3,12 @@ package vehicle;
 abstract class ElectricCar extends Car {
     /** Note: Car begins with a full charge (and thus range).
 @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
-public ElectricCar(String make, String model, double startingMileage,
-double milesOnMaxCharge)
+public ElectricCar(String make, String model, double startingMileage, double milesOnMaxCharge) {
+    
+}
 /** Defaults mileage to 0.
 @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
-public ElectricCar (String make, String model, double
-milesOnMaxCharge)
+public ElectricCar (String make, String model, double milesOnMaxCharge)
 /** Drives the full given number of miles.
 @throws IllegalArgumentException if miles is negative.
 @throws IllegalArgumentException if miles is too high given the
