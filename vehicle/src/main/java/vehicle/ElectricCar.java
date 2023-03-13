267501840ffdeea8d6c48d3c03268d0f7bb8881b
J-Tell <fairyq007@hotmail.com>
2023-03-13 10:24:12 -0400

    trying to fix all the errors

--- vehicle/src/main/java/vehicle/ElectricCar.java
+++ vehicle/src/main/java/vehicle/ElectricCar.java
@@ -4,24 +4,38 @@ abstract class ElectricCar extends Car {
     /** Note: Car begins with a full charge (and thus range).
 @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
 public ElectricCar(String make, String model, double startingMileage, double milesOnMaxCharge) {
-    
+    super(make, model, milesOnMaxCharge);
+    make = this.getMake();
+    model = this.getModel();
 }
 /** Defaults mileage to 0.
 @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.*/
-public ElectricCar (String make, String model, double milesOnMaxCharge)
+public ElectricCar (String make, String model, double milesOnMaxCharge) {
+    super(make, model, milesOnMaxCharge);
+}
 /** Drives the full given number of miles.
 @throws IllegalArgumentException if miles is negative.
 @throws IllegalArgumentException if miles is too high given the
 current charge.*/
-public void drive(double miles)
+public void drive(double miles) {
+    //
+}
 /** Returns how many more miles the car can currently go without
 recharging. */
-public double getRemainingRange()
+public double getRemainingRange() {
+    //
+}
 /** Returns how many miles the car could go on a full charge. */
-public double getMaxRange()
+public double getMaxRange() {
+    //
+}
 /** Recharges the car to max range capability. */
-public void recharge()
+public void recharge() {
+    //
+}
 /** Decreases the amount of energy in the battery based by the number
 of miles passed as an argument. */
-protected void decreaseCharge(double miles)
+protected void decreaseCharge(double miles) {
+    //
+}
 }
