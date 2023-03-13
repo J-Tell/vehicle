f3c79eb89aa408b320a880543b85a0562877b703
J-Tell <fairyq007@hotmail.com>
2023-03-13 10:05:55 -0400

    chevy bird changes

--- vehicle/src/main/java/vehicle/ChevroletBird.java
+++ vehicle/src/main/java/vehicle/ChevroletBird.java
@@ -1,5 +1,25 @@
 package vehicle;
 
 public class ChevroletBird extends ElectricCar{
-    
+    /** Chevrolet Birds have a 250 mile range on a full charge. They
+    start with their wings retracted.*/
+    private double startingMileage;
+    private boolean openWings;
+    public ChevroletBird(double startingMileage) {
+        startingMileage = 250;
+        openWings = false;
+    }
+/** Defaults mileage to 0. */
+    public ChevroletBird() {
+        startingMileage = 0;
+    }
+/** Returns whether the wings are currently extended. */
+    public boolean checkWingsExtended() {
+        return openWings;
+    }
+/** Drives just like all other Electric Cars, except make sure that
+you retract your wings first (duh).
+Coding tip: Write this method to re-use the behavior of the
+superclass drive. Don’t copy-and-paste the same code here.*/
+    public void drive(double miles)
 }
