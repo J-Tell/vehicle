2dac09314fffc7c4ede6782beffc5ae2bf6468b5
Faizan Sayed <baa98989@gmail.com>
2023-03-16 10:41:04 -0400

    did stuf

--- vehicle/src/test/java/vehicle/Group3_1_HondaS3.java
+++ vehicle/src/test/java/vehicle/Group3_1_HondaS3.java
@@ -1,5 +1,10 @@
 package vehicle;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import javax.lang.model.util.ElementScanner14;
 import javax.sound.sampled.SourceDataLine;
 
@@ -9,14 +14,10 @@ import javax.sound.sampled.SourceDataLine;
  */
 
 public class Group3_1_HondaS3 {
-    HondaAccordian honda = new HondaAccordian(2018);
-    String tostring = honda.toString();
-    if(tostring != honda.modelYear + honda.super.toString())
-        System.out.println("fail (bad toString())");
-    else
-        System.out.println("pass");
 
-    HondaAccordian honda2 = new HondaAccordian(12, 2018);
-    System.out.println(honda2);
+    public void TestConstructor1() {
+        
+    }
+
 }
  
