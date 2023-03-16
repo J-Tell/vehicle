3f5723ca07ad1c4b2f51f86b51441edb4f2d7e1d
J-Tell <fairyq007@hotmail.com>
2023-03-16 10:31:55 -0400

    methods go brrrrr

--- vehicle/src/test/java/vehicle/Group3_1_ChevyS3.java
+++ vehicle/src/test/java/vehicle/Group3_1_ChevyS3.java
@@ -1,10 +1,31 @@
 package vehicle;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.sql.Driver;
+import org.junit.jupiter.api.Test;
+
 /*
  * Both constructors
  * checkWingExtended
  * drive
  */
 public class Group3_1_ChevyS3 {
+    public void chevyConstructorTest3() {
+
+    ChevroletBird bird = new ChevroletBird(0);
     
+    assertThrows(IllegalArgumentException.class, () -> {
+        bird.fly(-1);
+    }, "Flying mileage cannot be negative.");
+}
+public void chevyConstructorTest4() {
+}
+public void chevyCheckWingExtendedTest() {
+}
+public void chevyDriveTest() {
+}
+
 }
