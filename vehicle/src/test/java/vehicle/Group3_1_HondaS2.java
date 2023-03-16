7d922225f0636dcc3aa77ce0f48a5a08cd008630
J-Tell <fairyq007@hotmail.com>
2023-03-16 10:13:53 -0400

    help

--- vehicle/src/test/java/vehicle/Group3_1_HondaS2.java
+++ vehicle/src/test/java/vehicle/Group3_1_HondaS2.java
@@ -11,5 +11,10 @@ package vehicle;
  */
 
 public class Group3_1_HondaS2 {
-    
+    public void hondaDriveTest() {
+        HondaAccordian honda = new HondaAccordian(70, 2018);
+        //assertEquals(0, honda.getMileage(), 0.1, "Default mileage should be zero.");
+        double mile = honda.getMileage();
+
+    }
 }
