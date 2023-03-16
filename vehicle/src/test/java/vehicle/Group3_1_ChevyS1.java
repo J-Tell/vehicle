c2cb08b0d17e27828eaf9008b5bf585b8c0bb178
aBacIdEe <maxgong2007@gmail.com>
2023-03-16 10:31:44 -0400

    S1 chevy

--- vehicle/src/test/java/vehicle/Group3_1_ChevyS1.java
+++ vehicle/src/test/java/vehicle/Group3_1_ChevyS1.java
@@ -2,9 +2,11 @@ package vehicle;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.sql.Driver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 /*
@@ -21,29 +23,42 @@ public class Group3_1_ChevyS1 {
     @Test
     public void chevyConstructorTest1() {
         ChevroletBird bird = new ChevroletBird(0);
-    }
+        double range = bird.getRemainingRange();
 
-    @Test
-    public void chevyConstructorTest2() {
-        ChevroletBird bird = new ChevroletBird(0);
+        // canDrive
+        assertTrue(bird.canDrive(range), "You should be able to drive this amount");
+        assertFalse(bird.canDrive(range + 1), "You should not be able to drive this amount");
+        assertFalse(bird.canDrive(-1), "You should not be able to drive this amount");
+        // toString
+        assertEquals("", bird.toString(), "this is the toString test");
+        // getMileage
+        assertEquals(0, bird.getMileage(), "Your mileage should be 0");
+        // getMake
+        assertEquals("Chevrolet", bird.getMake(), "Your make should be Chevrolet");
+        // getModel
+        assertEquals("Bird", bird.getModel(), "Your model should be Bird");
+        // roadTrip
+        List<Double> roadtrip = new ArrayList<Double>();
+        roadtrip.add(range);
+        roadtrip.add(1.0);
 
-        assertFalse(bird.canFly(30), "canFly should be false");
-        assertThrows(IllegalArgumentException.class, () -> {
-            bird.canFly(-1);
-        }, "Flying mileage cannot be negative.");
+        assertEquals(1, bird.roadTrip(roadtrip), "you drive full range 1st day, unable to drive second day");
 
-        bird.fly(230);
-        assertEquals(250, bird.getMileage(), 0.1, "fly should be positive");
-        assertThrows(IllegalArgumentException.class, () -> {
-            bird.fly(-1);
-        }, "Flying mileage cannot be negative.");
-        assertThrows(IllegalArgumentException.class, () -> {
-            bird.fly(251);
-        }, "Flying mileage cannot be over the remaining range of the car.");
+
+        // assertFalse(bird.canFly(30), "canFly should be false");
+        // assertThrows(IllegalArgumentException.class, () -> {
+        //     bird.canFly(-1);
+        // }, "Flying mileage cannot be negative.");
+
+        // bird.fly(230);
+        // assertEquals(250, bird.getMileage(), 0.1, "fly should be positive");
+        // assertThrows(IllegalArgumentException.class, () -> {
+        //     bird.fly(-1);
+        // }, "Flying mileage cannot be negative.");
+        // assertThrows(IllegalArgumentException.class, () -> {
+        //     bird.fly(251);
+        // }, "Flying mileage cannot be over the remaining range of the car.");
         
-
-
-
         // assertFalse(bird.canFly(-1), "Flying -1 should fail.");
         // assertTrue(bird.canFly(1), "1 should succeed.");
 
