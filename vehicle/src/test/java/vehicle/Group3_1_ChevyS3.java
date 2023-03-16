24350346ec748b107b4fe3501cb620dd3d84e9fe
J-Tell <fairyq007@hotmail.com>
2023-03-16 10:40:17 -0400

    ahhhhhhhh

--- vehicle/src/test/java/vehicle/Group3_1_ChevyS3.java
+++ vehicle/src/test/java/vehicle/Group3_1_ChevyS3.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
  * drive
  */
 public class Group3_1_ChevyS3 {
+    @Test
     public void chevyConstructorTest3() {
 
     ChevroletBird bird = new ChevroletBird(0);
@@ -21,10 +22,16 @@ public class Group3_1_ChevyS3 {
         bird.fly(-1);
     }, "Flying mileage cannot be negative.");
 }
+@Test
 public void chevyConstructorTest4() {
 }
+@Test
 public void chevyCheckWingExtendedTest() {
-}
+    ChevroletBird bird = new ChevroletBird(0);
+
+    assertFalse(bird.checkWingsExtended(), "checkWingsExtended should be false");
+
+@Test
 public void chevyDriveTest() {
 }
 
