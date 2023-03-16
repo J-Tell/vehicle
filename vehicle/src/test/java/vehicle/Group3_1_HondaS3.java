8addf6cd2aa163eeec4fa68b401d27d4c70f1a3b
Faizan Sayed <baa98989@gmail.com>
2023-03-16 10:40:54 -0400

    did stuf

--- vehicle/src/test/java/vehicle/Group3_1_HondaS3.java
+++ vehicle/src/test/java/vehicle/Group3_1_HondaS3.java
@@ -1,10 +1,22 @@
 package vehicle;
 
+import javax.lang.model.util.ElementScanner14;
+import javax.sound.sampled.SourceDataLine;
+
 /*
  * toString
  * both constructors
  */
 
 public class Group3_1_HondaS3 {
-    
+    HondaAccordian honda = new HondaAccordian(2018);
+    String tostring = honda.toString();
+    if(tostring != honda.modelYear + honda.super.toString())
+        System.out.println("fail (bad toString())");
+    else
+        System.out.println("pass");
+
+    HondaAccordian honda2 = new HondaAccordian(12, 2018);
+    System.out.println(honda2);
 }
+ 
