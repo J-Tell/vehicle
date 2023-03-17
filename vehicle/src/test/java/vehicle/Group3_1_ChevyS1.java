a39d8922eb551acd4cf8f220b3ca7810a3b7d7d3
aBacIdEe <maxgong2007@gmail.com>
2023-03-17 10:33:26 -0400

    refactor cases

--- vehicle/src/test/java/vehicle/Group3_1_ChevyS1.java
+++ vehicle/src/test/java/vehicle/Group3_1_ChevyS1.java
@@ -21,23 +21,50 @@ import org.junit.jupiter.api.Test;
 
 public class Group3_1_ChevyS1 {
     @Test
-    public void chevyScenario1() {
+    public void canDriveTest() {
         ChevroletBird bird = new ChevroletBird(0);
         double range = bird.getRemainingRange();
 
-        // canDrive
         assertTrue(bird.canDrive(range), "You should be able to drive this amount");
         assertFalse(bird.canDrive(range + 1), "You should not be able to drive this amount");
         assertFalse(bird.canDrive(-1), "You should not be able to drive this amount");
-        // toString
+    }
+
+    @Test
+    public void toStringTest() {
+        ChevroletBird bird = new ChevroletBird(0);
+
         assertEquals("Chevrolet Bird (0.0 mi)", bird.toString(), "this is the toString test");
-        // getMileage
+    }
+
+    @Test
+    public void getMileageTest() {
+        ChevroletBird bird = new ChevroletBird(0);
+
         assertEquals(0, bird.getMileage(), "Your mileage should be 0");
-        // getMake
+
+    }
+
+    @Test
+    public void getMakeTest() {
+        ChevroletBird bird = new ChevroletBird(0);
+
         assertEquals("Chevrolet", bird.getMake(), "Your make should be Chevrolet");
-        // getModel
+    }
+
+    @Test
+    public void getModelTest() {
+        ChevroletBird bird = new ChevroletBird(0);
+
         assertEquals("Bird", bird.getModel(), "Your model should be Bird");
-        // roadTrip
+
+    }
+
+    @Test
+    public void roadTripTest() {
+        ChevroletBird bird = new ChevroletBird(0);
+        double range = bird.getRemainingRange();
+
         List<Double> roadtrip = new ArrayList<Double>();
         roadtrip.add(range);
         roadtrip.add(1.0);
--- vehicle/src/test/java/vehicle/Group3_1_ChevyS2.java
+++ vehicle/src/test/java/vehicle/Group3_1_ChevyS2.java
@@ -12,18 +12,29 @@ import org.junit.jupiter.api.Test;
 
 public class Group3_1_ChevyS2 {
     @Test
-    public void chevyScenario2() {
+    public void getRemainingRangeTest() {
+        ChevroletBird bird = new ChevroletBird(0);
+        bird.drive(bird.getRemainingRange());
+        // getRemainingRange
+        assertEquals(250, bird.getRemainingRange(), "Should have 250 miles of range");
+        //
+    }
+
+    @Test
+    public void getMaxRangeTest() {
+        ChevroletBird bird = new ChevroletBird(0);
+        bird.drive(bird.getRemainingRange());
+        // getMaxRange
+        assertEquals(250, bird.getMaxRange(), "Should be 250 miles");
+    }
+
+    @Test
+    public void rechargeTest() {
         ChevroletBird bird = new ChevroletBird(0);
         bird.drive(bird.getRemainingRange());
         
         // recharge
         bird.recharge();
-        // getRemainingRange
-        assertEquals(250, bird.getRemainingRange(), "Should have 250 miles of range");
-        // getMaxRange
-        assertEquals(250, bird.getMaxRange(), "Should be 250 miles");
-        //
-
     }
     
 }
