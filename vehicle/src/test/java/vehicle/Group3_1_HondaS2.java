886302287ad405ed032fb8b302e51237fc8c6cb1
aBacIdEe <maxgong2007@gmail.com>
2023-03-17 10:25:45 -0400

    put in test cases

--- vehicle/src/test/java/vehicle/Group3_1_HondaS2.java
+++ vehicle/src/test/java/vehicle/Group3_1_HondaS2.java
@@ -1,5 +1,7 @@
 package vehicle;
 
+import org.junit.jupiter.api.Test;
+
 /*
  * Need to test methods:
  * getMPG
@@ -11,10 +13,30 @@ package vehicle;
  */
 
 public class Group3_1_HondaS2 {
-    public void hondaDriveTest() {
+    @Test
+    public void driveTest() {
         HondaAccordian honda = new HondaAccordian(70, 2018);
         //assertEquals(0, honda.getMileage(), 0.1, "Default mileage should be zero.");
         double mile = honda.getMileage();
+    }
+
+    @Test
+    public void getMPGTest() {
+
+    }
+
+    @Test
+    public void getFuelLevelTest() {
+
+    }
+
+    @Test
+    public void refillTankTest() {
+
+    }
+
+    @Test
+    public void getRemainingRangeTest() {
 
     }
 }
