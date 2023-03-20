ff9f0e99dbab13babefccb6e4fec0e95f481c4dd
aBacIdEe <maxgong2007@gmail.com>
2023-03-20 09:59:57 -0400

    some little imports

--- vehicle/src/test/java/vehicle/Group3_1_ChevyS4.java
+++ vehicle/src/test/java/vehicle/Group3_1_ChevyS4.java
@@ -1,10 +1,8 @@
 package vehicle;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.sql.Driver;
+
 import org.junit.jupiter.api.Test;
 
 
